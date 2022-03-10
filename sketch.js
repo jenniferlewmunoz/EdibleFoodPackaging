67a98337d4af5c9c8d9d8e5267127cbbd5d38bb2
Jennifer <jenniferlewmunoz@gmail.com>
2022-03-10 02:18:30 -0800

    Finished scene1

--- sketch.js
+++ sketch.js
@@ -21,6 +21,12 @@ var clickables;                    // an array of clickable objects
 var currentStateName = "";
 var moodImage;
 
+// Player data: Scientist, Mackey, Owner, Mayor, Citizen
+var reason;
+var icons = [];
+var scores = [10, 0, 0, 0, 0];
+var unlocked = [true, false, false, false, false];
+
 // Color
 var bkColor = '#031927';
 var textColor = '#E9D6EC';
@@ -54,6 +60,14 @@ function preload() {
   scientist[1] = loadImage('assets/scientist_upset.png');
   scientist[2] = loadImage('assets/scientist_wave_left.png');
   scientist[3] = loadImage('assets/scientist_wave_right.png');
+
+  // Preload icon
+  icons[0] = loadImage('assets/scientist_icon.png');
+  icons[1] = loadImage('assets/mackey_icon.png');
+  icons[2] = loadImage('assets/owner_icon.png');
+  icons[3] = loadImage('assets/mayor_icon.png');
+  icons[4] = loadImage('assets/citizen_icon.png');
+  icons[5] = loadImage('assets/lock_icon.png');
 }
 
 // Setup code goes here
@@ -77,7 +91,6 @@ function setup() {
 
   //textAlign(CENTER, CENTER);
 }
-
 
 // Draw code goes here
 function draw() {
@@ -154,39 +167,51 @@ function drawImage() {
   }
 }
 
+function drawUI() {
+  clickablesManager.draw();
+}
+
+// Draws a box without text
+function drawBox(x, y, w, h) {
+  fill(232, 232, 230, 170);
+  strokeWeight(6);
+  rect(x, y, w, h, 40);
+}
+
+// Draws a box with text
+function drawTextBox(x, y, w, h, fontSize, message) {
+  drawBox(x, y, w, h);
+
+  // Style text
+  fill(0);
+  textSize(fontSize);
+  textAlign(CENTER);
+  textFont(titleFont);
+
+  // Write/Format text
+  let padding = 40;
+  text(message, x + padding, y + padding, w - padding * 2, h - padding * 2);
+}
+
 function drawOther() {
   push();
-
-  if (currentStateName == "Splash") {
-    drawSplashScreen();
-  }
-
-  if (currentStateName == "Intro1") {
-    drawIntro1();
-  }
-
-  // Draw mood — if not on Splash or Instructions screen  
-  if (currentStateName !== "Splash" && currentStateName !== "Instructions") {
-    fill(color(textColor));
-    textFont(buttonFont);
-    textSize(24);
-    text(currentStateName, width / 2, 50);
-  }
-
+  if (currentStateName == "Splash") drawSplashScreen();
+  if (currentStateName == "Intro1") drawIntro1();
+  if (currentStateName == "Intro2") drawIntro2();
+  if (currentStateName == "Intro3") drawIntro3();
+  if (currentStateName == "Scene1") drawScene1();
+  if (currentStateName == "EndGame") drawEndGame();
   pop();
 }
 
-//-- right now, it is just the clickables
 function drawUI() {
   clickablesManager.draw();
 }
 
 function drawSplashScreen() {
 
-  // Make rectangle
-  fill(232, 232, 230, 170);
-  strokeWeight(20);
-  rect(70, 70, 600, 450, 40);
+  // Draw rect for title
+  drawBox(70, 70, 600, 450);
 
   // Write title
   fill(0);
@@ -214,8 +239,87 @@ function drawSplashScreen() {
   }
 }
 
-function drawIntro1() {
+function drawEndGame() {
+
+  // Draw UI
+  drawScientist(1, 250);
+  drawBox(850, 40, 350, 600);
+
+  // Draw reason textbox
+  drawTextBox(450, 40, 350, 400, 20, reason);
+
+  // Draw text
+  textSize(30);
+  fill(0);
+  text("End game results:", 1020, 100);
+
+  // Draw data
+  let yPos = 200;
+  let meterY = 190;
+  icons[5].resize(70, 70);
+
+  for (i = 0; i < icons.length - 1; i++) {
+
+    if (unlocked[i]) {
+      // Draw icon
+      icons[i].resize(70, 70);
+      image(icons[i], 910, yPos);
+
+      // Draw meter
+      fill(255);
+
+      let meterX = 960;
+      let score = scores[i];
+
+      for (j = score; score > 0; score = score - 10) {
+        rect(meterX, meterY, 20, 20);
+        meterX = meterX + 20;
+      }
+    } else {
+      image(icons[5], 910, yPos);
+    }
+    yPos += 90;
+    meterY = meterY + 90;
+  }
+}
+
+function drawIconBox() {
+  scientistIcon.resize(60, 60);
+  image(scientistIcon, 900, 160);
+}
+
+function drawScientist(index, xcord) {
   let imgSize = 700;
-  scientist[3].resize(imgSize, imgSize);
-  image(scientist[3], 300, height - (imgSize / 2));
+  scientist[index].resize(imgSize, imgSize);
+  image(scientist[index], xcord, height - (imgSize / 2));
+}
+
+function drawIntro1() {
+  drawScientist(3, 300);
+  let message = "Ready to embark on an adventure to a plastic-free world?";
+  drawTextBox(620, 130, 470, 300, 45, message);
+}
+
+function drawIntro2() {
+  drawScientist(0, 300);
+  let message = "It's 2050, and we’ve finally created a great replacement for single-use plastic that wraps most our foods today. Edible food packaging would serve our environment in ways we can’t even imagine! Although we have lot’s of supporters, we have a lot of people trying to stop a mandate for edible food packaging here in San Francisco."
+  drawTextBox(620, 130, 470, 290, 20, message);
+}
+
+function drawIntro3() {
+  drawScientist(2, 900);
+  let message = "Otherwise, I’m the lead scientist Albert at EFP Labs, we are the first to create edible food packaging that has all the properties to work in today’s world!\n\n\nWill you join us to reduce the production of single-use plastics and change the world as we know it today?";
+  drawTextBox(120, 130, 500, 302, 20, message);
+
+  reason = "By 2200, due to plastic littered oceans all sea life becomes extinct, and the overwhelming amount of plastic on land has decomposed and released plastic toxins that posion our soils leading to the extinction of the human population.";
+}
+
+function drawScene1() {
+  drawScientist(0, 300);
+  drawBox(530, 210, 600, 250);
+  fill(0);
+  textSize(100);
+  text("Scene One:", 570, 350);
+  textSize(40);
+  text("The Logistics", 710, 400);
 }
